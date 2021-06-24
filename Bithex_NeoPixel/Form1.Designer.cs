96c62565788872a2ccc9a8ed70961b4213e4e95c
MyKanki <86431755+MyKanki@users.noreply.github.com>
2021-06-24 21:41:54 +0300

    Add files via upload

---
+++ Bithex_NeoPixel/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace Bithex_NeoPixel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.açBitmap24ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.hakkındaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(900, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.açBitmap24ToolStripMenuItem,
+            this.çıkToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // açBitmap24ToolStripMenuItem
+            // 
+            this.açBitmap24ToolStripMenuItem.Name = "açBitmap24ToolStripMenuItem";
+            this.açBitmap24ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.açBitmap24ToolStripMenuItem.Text = "Aç (Bitmap24)";
+            this.açBitmap24ToolStripMenuItem.Click += new System.EventHandler(this.açBitmap24ToolStripMenuItem_Click);
+            // 
+            // çıkToolStripMenuItem
+            // 
+            this.çıkToolStripMenuItem.Name = "çıkToolStripMenuItem";
+            this.çıkToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.çıkToolStripMenuItem.Text = "Çık";
+            this.çıkToolStripMenuItem.Click += new System.EventHandler(this.çıkToolStripMenuItem_Click);
+            // 
+            // hakkındaToolStripMenuItem
+            // 
+            this.hakkındaToolStripMenuItem.Name = "hakkındaToolStripMenuItem";
+            this.hakkındaToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.hakkındaToolStripMenuItem.Text = "Hakkında";
+            this.hakkındaToolStripMenuItem.Click += new System.EventHandler(this.hakkındaToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 49);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(876, 309);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 364);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Bithex Neopixel";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem açBitmap24ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Bithex_NeoPixel/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bithex_NeoPixel
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap image1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hakkındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu Program MyKanki Tarafından 24/06/2021 tarinde yazılmıştır. \n" +
+                "Programın amacı Neopixel 16x16 led sistemi için bitmap resim kodunu 16 bitlik" +
+                "hex koduna dönüştürmeye yarar. \n" +
+                "Tüm Hakları MyKanki ye aittir ve onun güvencesi altındadır. \n" +
+                "Version 1.0");
+        }
+
+        private void açBitmap24ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Bitmap24 Dosyası (.bmp)|*.bmp";
+            file.FilterIndex = 1;
+            file.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            file.RestoreDirectory = true;
+            file.CheckFileExists = false;
+            file.Title = "Bitmap24 Dosyası Seçiniz...";
+            file.Multiselect = false;
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                string DosyaYolu = file.FileName;
+                string DosyaAdi = file.SafeFileName;
+                try
+                {
+                    // Retrieve the image.
+                    image1 = new Bitmap(DosyaYolu, true);
+
+                    // Loop through the images pixels to reset color.
+
+                    for (int str = 0; str < 16; str++)
+                    {
+                        if (str % 2 == 0)
+                        {
+                            for (int stn = 0; stn < 16; stn++)
+                            {
+                                Color pixelColor = image1.GetPixel(str, stn);
+                                string hexValue = pixelColor.R.ToString("X2") + pixelColor.G.ToString("X2") + pixelColor.B.ToString("X2");
+                                richTextBox1.AppendText("0x" + hexValue + ",");
+                            }
+                        }
+                        else
+                        {
+                            for (int stn = 15; stn >= 0; stn--)
+                            {
+                                Color pixelColor = image1.GetPixel(str, stn);
+                                string hexValue = pixelColor.R.ToString("X2") + pixelColor.G.ToString("X2") + pixelColor.B.ToString("X2");
+                                richTextBox1.AppendText("0x" + hexValue + ",");
+                            }
+                        }
+                        richTextBox1.AppendText("\n");
+                    }
+                    pictureBox1.Image = image1;
+                    if (image1.PixelFormat.ToString() != "Format24bppRgb")
+                    {
+                        MessageBox.Show("Resim Formatı 24 bitlik bitmap formatı değil renk sorunu yaşayabilirsiniz!");
+                    }
+
+                    //label1.Text = "Pixel format: " + image1.PixelFormat.ToString();
+                }
+                catch (ArgumentException)
+                {
+                    MessageBox.Show("There was an error. Check the path to the image file.");
+                }
+            }
+        }
+
+        private void çıkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
